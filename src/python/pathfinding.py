e6ec00418f3adb35b794eba40f53e69a48ee0453
Fra <rakouett-du-56@hotmail.fr>
2023-11-11 08:18:58 +0100

    Take ledges and signs into account for pathfinding process

--- src/python/pathfinding.py
+++ src/python/pathfinding.py
@@ -17,11 +17,14 @@ CELL_COST = {
 
 SOLID_BLOCKS = [
     "X", # Wall, NPC
-    "S", # Sign
-    "D", # One-way ledge to go down
-    "L", # One-way ledge to go left
-    "U", # One-way ledge to go up
-    "R"  # One-way ledge to go right
+    "S"  # Sign
+]
+
+DIRECTIONS = [
+    {"orientation": (0, -1), "solidLedges": ["D","U","R"]}, # Left
+    {"orientation": (0, 1),  "solidLedges": ["D","L","U"]}, # Right
+    {"orientation": (-1, 0), "solidLedges": ["D","L","R"]}, # Up
+    {"orientation": (1, 0),  "solidLedges": ["L","U","R"]}  # Down
 ]
 
 # Node class for A* Pathfinding
@@ -34,6 +37,9 @@ class Node():
         self.g = 0
         self.h = 0
         self.f = 0
+
+        # Special process for ledge nodes since we don't actually step on them
+        self.isLedge = False
 
     def __str__(self):
         return (
@@ -76,19 +82,29 @@ def astar(maze, start, end):
             current = current_node
 
             while current is not None:
-                path.append(current.position)
+                # Don't add ledge position to the path, we don't walk on the ledge, only go through it
+                # The optimal way would be to process ledge cells during input frame calculation
+                if (not current.isLedge):
+                    path.append(current.position)
+
                 current = current.parent
-            return path[::-1] # Return reversed path
+
+            # Return reversed path
+            return path[::-1]
 
         # Generate children
         children = []
-        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]: # Adjacent squares
+        for new_position in DIRECTIONS: # Adjacent squares
 
             # Get node position
-            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+            node_position = (current_node.position[0] + new_position["orientation"][0], current_node.position[1] + new_position["orientation"][1])
 
             # Can't walk through solid blocks
-            if maze[node_position[0]][node_position[1]] in SOLID_BLOCKS:
+            if maze[node_position[0]][node_position[1]] in SOLID_BLOCKS + new_position["solidLedges"]:
+                continue
+
+            # Don't go up if a sign is just above since it triggers a dialogue
+            if (maze[node_position[0] - 1][node_position[1]] == "S" and new_position["orientation"] == (-1, 0)):
                 continue
 
             # We can walk through the block : add node to the children list
@@ -104,10 +120,13 @@ def astar(maze, start, end):
 
             childPositionY = child.position[0]
             childPositionX = child.position[1]
-            cellValue = maze[childPositionY][childPositionX]
+            cellType = maze[childPositionY][childPositionX]
+
+            # Special process for ledge cells, see path processing
+            child.isLedge = (cellType in ["L","U","R","D"])
 
             # Create the f, g, and h values
-            child.g = current_node.g + CELL_COST.get(cellValue, 999) # Default cell cost is 999, basically solid block
+            child.g = current_node.g + CELL_COST.get(cellType, 999) # Default cell cost is 999, basically solid block
             child.h = abs(childPositionY - end_node.position[0]) + abs(childPositionX - end_node.position[1]) # Manhattan distance
             child.f = child.g + child.h
 
@@ -119,6 +138,8 @@ def astar(maze, start, end):
             open_list.append(child)
 
 def getPathCoordinates(startPosition, endPosition):
+
+    # Get most effective path from startPosition to endPosition
     path = astar(PLATINUM_MAP, (startPosition.Y, startPosition.X), (endPosition.Y, endPosition.X))
     pathInputSequence = ""
 
