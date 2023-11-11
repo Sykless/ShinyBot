aaf382e7cba84be63f4ba806e4dfae2ef7095d12
Fra <rakouett-du-56@hotmail.fr>
2023-11-11 20:11:27 +0100

    Run while moving player

--- src/python/joypad.py
+++ src/python/joypad.py
@@ -15,3 +15,23 @@ def writeInput(inputSequence, endSequence = None):
     
     print(frameByFrameInputSequence)
     memory.writeMemoryData("joypad", frameByFrameInputSequence)
+
+def writeRunInput(pathInputSequence, playerDirection):
+
+    if (pathInputSequence):
+        # Determine how many frames the first input needs to be pressed
+        frameByFrameInputSequence = pathInputSequence[0] * ( 
+            5 # 5 frames of input lag
+        + 6 * (pathInputSequence[0] != playerDirection) # 6 frames to turn around
+        + 6 # 6 frames to start run animation
+        - 5 # -5 frames to change direction at frame 1 of start animation
+        )
+
+        # Process the rest of the inputs
+        for inputButton in pathInputSequence[1:]:
+            # 8 frames per input during run animation
+            frameByFrameInputSequence += 8 * inputButton
+
+        # Set run flag to true and write input sequence
+        memory.setMemoryFlag(runFlag = True)
+        memory.writeMemoryData("joypad", frameByFrameInputSequence)
