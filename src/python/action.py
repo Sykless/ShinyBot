cf95aae5371cec112922042254ca1177ec17479c
Fra <rakouett-du-56@hotmail.fr>
2024-07-16 21:07:27 +0200

    Mash B on menu opening if not on overworld

--- src/python/action.py
+++ src/python/action.py
@@ -5,50 +5,46 @@ import memory
 from utils import waitFrames
 
 def openMenu():
-    openMenuTries = 0
+
+    # If not on overworld, don't even try to open menu, just mash B
+    openMenuTries = 3 * int(img.poketch.isOnScreen(img.getScreenshot()))
     mashExitTries = 0
     waitAfterPress = False
 
     while True:
-        # Check input previsouy saved
-        joypadInput = memory.readJoypadData()
-
         # Only apply new input if no input is found in memory
-        if (len(joypadInput) == 0):
+        if (len(memory.readJoypadData()) == 0):
             screenshot = img.getScreenshot()
+            menuPosition = img.getMenuPosition(screenshot)
 
-            # Overworld
-            if (img.poketch.isOnScreen(screenshot)):
-                menuPosition = img.getMenuPosition(screenshot)
+            # Menu is not open
+            if (menuPosition == 0):
 
-                # Menu is not open
-                if (menuPosition == 0):
+                # Wait for potential dialogue closing time
+                if (waitAfterPress):
+                    waitFrames(10) #  Wait 10 frames
+                    waitAfterPress = False
 
-                    # Wait for potential dialogue closing time
-                    if (waitAfterPress):
-                        waitFrames(10) #  Wait 10 frames
-                        waitAfterPress = False
+                # Try to open menu the regular way
+                elif (openMenuTries < 3):
+                    joypad.writeInput("X") # Press X to open menu
+                    openMenuTries += 1
+                    waitAfterPress = True
 
-                    # Try to open menu the regular way
-                    elif (openMenuTries < 3):
-                        joypad.writeInput("X") # Press X to open menu
-                        openMenuTries += 1
-                        waitAfterPress = True
+                # Menu won't open, maybe stuck in dialogue
+                elif (mashExitTries < 3):
+                    joypad.writeInput("BBBBBBBBBBX") # Mash B to exit then try pressing X again
+                    mashExitTries += 1
+                    waitAfterPress = True
 
-                    # Menu won't open, maybe stuck in dialogue
-                    elif (mashExitTries < 3):
-                        joypad.writeInput("BBBBBBBBBBX") # Mash B to exit then try pressing X again
-                        mashExitTries += 1
-                        waitAfterPress = True
-
-                    # Menu definitely won't open, go back to main loop
-                    else:
-                        return 0
-
-                # Menu open, exit function
+                # Menu definitely won't open, go back to main loop
                 else:
-                    return menuPosition
-                        
+                    return 0
+
+            # Menu open, exit function
+            else:
+                return menuPosition
+            
             # Not in overworld : go back to main loop
             else:
                 return 0
