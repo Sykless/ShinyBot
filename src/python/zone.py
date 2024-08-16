f510f5ad29eb24234ebd1b206a6506a8cb03e91c
Fra <rakouett-du-56@hotmail.fr>
2024-08-16 11:20:09 +0200

    We might share the position of a NPC : don't stop process

--- src/python/zone.py
+++ src/python/zone.py
@@ -163,7 +163,7 @@ class City():
 # Check if position if a valid cell (reachable + in map bounds)
 def checkPositionValidity(position, zoneMap = None):
     try:
-        if ((position.zone.map if zoneMap is None else zoneMap)[position.Y][position.X] in ["X","N","I","s"," "]):
+        if ((position.zone.map if zoneMap is None else zoneMap)[position.Y][position.X] in ["X","I","s"," "]):
             print("Unreachable cell : " + str(position) + " (" + (position.zone.map if zoneMap is None else zoneMap)[position.Y][position.X] + ")")
             return False
     except IndexError:
