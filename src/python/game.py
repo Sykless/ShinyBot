0a7d47dcf416e5accae27d067e6bd033fab00c02
Fra <rakouett-du-56@hotmail.fr>
2025-01-12 19:33:51 +0100

    Retrieve Feebas tiles and fog from memory

--- src/python/game.py
+++ src/python/game.py
@@ -3,17 +3,26 @@ import bag
 import memory
 
 from encounter import SPECIALGRASSENCOUNTERS
-from data import POKEMON_NAMES
-from data import GBAGAME_NAMES
+from data import POKEMON_NAMES, GBAGAME_NAMES
+from zone import MONTCOURONNE_SALLE8, Position
 
 CLOSEBAGMENU = 24
+FEEBAS_ROCK_POSITIONS = [51, 56, 184, 203, 203, 203, 203, 203, 203, 203, 214, 214, 214,
+                         214, 214, 214, 214, 225, 225, 225, 225, 225, 225, 225, 230, 230,
+                         234, 234, 234, 234, 234, 234, 234, 239, 239, 243, 243, 243, 243,
+                         243, 243, 243, 254, 254, 254, 254, 254, 254, 254, 265, 265, 265,
+                         265, 265, 265, 265, 280, 280, 296, 296, 349, 349, 349, 364, 364,
+                         364, 379, 379, 379, 409, 419, 419, 419, 434, 434, 434, 449, 449,
+                         449, 455, 455, 471, 471, 477]
 
 class Game:
-    def __init__(self, hourOfDay, repelSteps, selectedBagSection, selectedBagItemId, registeredKeyItem, swarmPokemon, marshPokemonList, gardenPokemonToday, gardenPokemonYesterday, gbaGame, encounterTables):
+    def __init__(self, hourOfDay, repelSteps, selectedBagSection, selectedBagItemId, registeredKeyItem, isFoggy, feebasSeed, swarmPokemon, marshPokemonList, gardenPokemonToday, gardenPokemonYesterday, gbaGame, encounterTables):
         self.hourOfDay = hourOfDay
         self.repelSteps = repelSteps
         self.selectedBagSection = None
         self.registeredKeyItem = registeredKeyItem
+        self.isFoggy = isFoggy
+        self.feebasSeed = feebasSeed
         self.swarmPokemon = swarmPokemon
         self.marshPokemonList = marshPokemonList
         self.gardenPokemonToday = gardenPokemonToday
@@ -40,6 +49,19 @@ class Game:
             # Retrieve item as usual
             else:
                 self.selectedBagItem = bag.getItemFromBagId(selectedBagSection, selectedBagItemId)
+
+    def getFeebasTiles(self):
+        feebasTiles = []
+
+        # Find Feebas tiles from the Feebas seed, excluding rocks position
+        for i in range(4):
+            feebasPosition = (((self.feebasSeed >> (24 - 8*i)) & 0xff) % 0x84) + 0x84*i
+            feedbasAdjustedPosition = feebasPosition + sum(1 for rockPosition in FEEBAS_ROCK_POSITIONS if rockPosition <= feebasPosition)
+            
+            # Convert position (0 -> 611) to Position object with X,Y coordinates
+            feebasTiles.append(Position(9 + feedbasAdjustedPosition % 18, 18 + feedbasAdjustedPosition // 18, MONTCOURONNE_SALLE8))
+
+        return feebasTiles
 
     def displayEncounters(self):
         for zoneType, encounterTable in self.encounterTables.items():
